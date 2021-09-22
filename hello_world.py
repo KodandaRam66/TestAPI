5fc1a95982bc0885911718b406af07f180f7aec0
Kodanda ram <v.kondandaramu.90@gmail.com>
2021-09-22 19:50:05 +0530

    updated hello_world.py and index.html

--- hello_world.py
+++ hello_world.py
@@ -10,14 +10,45 @@ def get_hello_world():
 def greetings():
     if request.method == "POST":
         user = request.form["name"]
-        return redirect(url_for("user", usr=user))
+        target = request.form["targets"]
+        print(user)
+        if user == "":
+            return render_template("test.html")
+        elif target == "greetings":
+            return redirect(url_for("greet", usr=user))
+        elif target == "task1":
+            return redirect(url_for("task_a", usr=user))
+        elif target == "task2":
+            return redirect(url_for("task_b", usr=user))
+        elif target == "task3":
+            return redirect(url_for("task_c", usr=user))
+        
     else:
         return render_template("test.html")
 
 @app.route("/<usr>")
-def user(usr):
-    return f"<h1>Hello {usr}!!</h1>"
+def greet(usr):
+    return f'''<h1>Hello {usr}!!</h1><br>
+    <p><a href="/test"><button >BACK</button></a></p>'''
 
+@app.route("/task1/<usr>")
+def task_a(usr):
+    return f'''<h1>{usr}, you are now seeing task 1 page</h1><br>
+    <p>Need to wait for some time...That's your task!!</p><br>
+    <a href="/test"><button >BACK</button></a>'''
+
+@app.route("/task2/<usr>")
+def task_b(usr):
+    return f'''<h1>Hey {usr}, Now you are in task 2!</h1><br>
+    <p>You are early here!</p>
+    <p>please come back later</p>
+    <a href="/test"><button >BACK</button></a>'''
+
+@app.route("/task3/<usr>")
+def task_c(usr):
+    return f'''<h1>{usr}, </h1><br>
+    <p>I couldn't find what to write in here!! Hope you like this</p>
+    <a href="/test"><button >BACK</button></a>'''
 
 if __name__ == "__main__":
     app.run(debug=True)
